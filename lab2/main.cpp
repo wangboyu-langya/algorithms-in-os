44a0b416b219fcabede118803fc7c05a812359ab
huxianglong <hxianglong@gmail.com>
2018-06-24 22:41:09 -0400

    change the frame

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -15,12 +15,14 @@
 
 using namespace std;
 
-bool if_verbose; // whether verbose output
-bool IF_DEBUG = true;
-int quantum; // quantum for RR and PRIO only
-std::ifstream ifs, rfs; // the input file
-vector<int> rand_v; // random vector
-vector<int>::iterator rand_it;
+
+void init(int argc, char *argv[]);
+
+void load_rand(string &rand_name);
+
+int my_random(int burst);
+
+
 // scheduler type
 enum sch {
     FCFS, LCFS, SJF, RR, PRIO
@@ -32,7 +34,7 @@ enum state {
 };
 
 enum event_type {
-    C2Re, Re2Ru, Ru2B, B2Re, Ru2Re, Done
+    C2Re, Re2Ru, Ru2B, B2Re, Ru2Re, Done, Creat, Empty
 };
 
 class Process {
@@ -47,7 +49,9 @@ public:
     int cpu_burst;
     int io_burst;
 
-    Process(int p, int at, int tc, int cb, int ib, int pr) {
+    Process() = default;
+
+    Process(int p, int at = 0, int tc = 0, int cb = 0, int ib = 0, int pr = 0) {
         pid = p;
         arrival = at;
         total_cpu = tc;
@@ -62,17 +66,34 @@ public:
     }
 };
 
+Process empty_process; // and empty process
+
 class Event {
 public:
+    int creat; // time the event should be triggered
     int trigger; // time the event should be triggered
+    int cb;
+    int ib;
     Process &process; // process the event is attached to
-    event_type &type; // event type
-    Event(int t, Process &p, event_type &tp) {
-        trigger = t;
-        process = p;
-        type = tp;
+    event_type type; // event type
+    Event(int t1, int t2, int c, int i, Process &p, event_type tp) : creat(t1), trigger(t2), cb(c), ib(i), process(p),
+                                                                     type(tp) {
+    }
+
+    void print() {
+        switch (type) {
+            case C2Re:
+                cout << trigger << ' ' << process.pid << ' ' << trigger - creat << ':' << "CREATED -> READY" << endl;
+                break;
+            case Re2Ru:
+                cout << trigger << ' ' << process.pid << ' ' << trigger - creat << ':' << "READY -> RUNNG" <<
+                     ' ' << process.cpu_burst << endl;
+                break;
+        }
     }
 };
+
+Event empty_event = Event(0, 0, 0, 0, empty_process, Empty); // and empty process
 
 class Des {
 public:
@@ -80,7 +101,8 @@ public:
     // with list insert takes O(1), search takes O(n)
     // so basically they are the same
     // here I choose list sice this is easier to implement
-    list<&Event> events;
+    list<Event &> events;
+    list<Event &> expired;
 
     // insert an event in ascending order
     void insert(Event &e) {
@@ -88,9 +110,9 @@ public:
             events.push_back(e);
         else {
             // the event before
-            list<&Event>::iterator bef = events.begin();
+            list<Event &>::iterator bef = events.begin();
             // the event after
-            list<&Event>::iterator aft = events.begin();
+            list<Event &>::iterator aft = events.begin();
             aft++;
             // case 1: insert the event before the previous one if earlier
             while (bef != events.end())
@@ -126,14 +148,92 @@ public:
 
     // return the first element of the list and delete it
     // TODO: need to check the reference is valid
-    Event& pop() {
-        Event &e = events.front();
+    Event pop() {
+        Event e = events.front();
         events.pop_front();
         return e;
     }
 };
 
+
+class Scheduler {
+public:
+    list<Process &> ps; // list of all the processes
+    Event ce = empty_event; // current event
+    Process &cp = empty_process; // current process
+    Scheduler() = default;
+
+    Scheduler(vector<Process> p) {
+        for (auto &r : p)
+            ps.push_back(r);
+    }
+
+    // next event based on the finished event
+    virtual Event next(Event &e);
+
+    // whether the scheduler finishes
+    bool running() {
+        if (ps.empty())
+            return false;
+        else
+            return true;
+    }
+};
+
+class Scheduler_fcfs : public Scheduler {
+public:
+    vector<Process &> running; // running processes
+    vector<Process &> ready; // ready processes
+    Scheduler_fcfs(vector<Process> &p) : Scheduler(p) {}
+
+    Event next(Event &e) override {
+        switch (e.type) {
+            case Creat:
+                e.print();
+                cp = ps.front();
+                ce = Event(cp.arrival, cp.arrival, -1, -1, cp, C2Re);
+                return ce;
+            case C2Re:
+                e.print();
+                // a new process is ready
+                cp = e.process;
+                // if no process is running
+                if (running.empty())
+                    ce = Event(cp.arrival, cp.arrival, -1, -1, cp, Re2Ru);
+                // otherwise, push it to ready state
+                else
+                    ready.push_back(cp);
+                    ce = empty_event;
+                return ce;
+            case Re2Ru:
+                cp = ps.front();
+                // really allocate the cpu and print
+                e.cb = my_random(cp.io_burst);
+                e.print();
+                // insert it to running
+                if (running.empty())
+                    running.push_back(cp);
+                ce = Event(e.creat, e.trigger, -1, my_random(cp.io_burst), cp, Ru2B);
+        }
+    }
+};
+
+
+// global variables
+bool if_verbose; // whether verbose output
+bool IF_DEBUG = true;
+int quantum; // quantum for RR and PRIO only
+std::ifstream ifs, rfs; // the input file
+vector<int> rand_v; // random vector
+vector<int>::iterator rand_it;
+Scheduler schdulr;
+Des des;
 vector<Process> processes;
+//unordered_map<event_type , string> event_map{
+//        {C2Re, "CREATED -> READY"},
+//        {Re2Ru, "READY -> RUNNG"},
+//        {Re2Ru, "READY -> RUNNG"},
+//};
 
 void load_rand(string &rand_name) {
     // read random number into an array
@@ -162,6 +262,7 @@ int my_random(int burst) {
         rand_it = rand_v.begin();
     return *rand_it++ % burst + 1;
 }
+
 
 // init the arguments and input files
 void init(int argc, char *argv[]) {
@@ -221,20 +322,31 @@ void init(int argc, char *argv[]) {
         processes.push_back(Process(i, a[0], a[1], a[2], a[3], my_random(4)));
         i++;
     }
+    // init the scheduler
+    switch (sch_type) {
+        case FCFS:
+            schdulr = Scheduler_fcfs(processes);
+    }
+    // init the DES layer
+//    des.insert(Event(0, 0, 0, 0, empty_process, Creat));
+    for (Process &s : processes)
+        des.insert(Event(s.arrival, s.arrival, -1, -1, s, Creat));
     if (IF_DEBUG)
         for (Process &s : processes)
             s.print();
 }
 
-class Scheduler {
-public:
-    vector<Process&> processes;
-    virtual Event &next();
-};
-
-class Schdulr_fcfs : public Scheduler{
-}
-
 int main(int argc, char *argv[]) {
     init(argc, argv);
+    // the whole of simulation
+    // TODO: I first assume there is no spontanous event
+    while (schdulr.running()) {
+        // the next event
+        Event e1 = des.pop();
+        // if the event is not empty, call the scheduler
+        if (e1.type != Empty) {
+            Event e2 = schdulr.next(e1);
+            des.insert(e2);
+        }
+    }
 }
